047ac105d0018990d57ec1a56552b04559829842
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-13 12:17:49 -0500

    :sparkles: Import functions in init

--- playground/closures/__init__.py
+++ playground/closures/__init__.py
@@ -2,6 +2,6 @@
 This tells Python that this is a module.
 """
 
-from .incrementer import INCREMENT_BY_10
-from .multiplier import FIVE_TIMES
-from .printer import say_cheese
+from .incrementer import increment_by, INCREMENT_BY_10
+from .multiplier import make_multiplier_of, FIVE_TIMES, THREE_TIMES
+from .printer import print_msg, say_hello, say_cheese
